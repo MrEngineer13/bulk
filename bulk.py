09662d5827b17be51889e2ceb7580e291c6332e5
MrEngineer13 <mrengineer13@live.com>
2020-03-27 12:44:46 -0400

    Added replacement text capabilities

--- bulk.py
+++ bulk.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 # import libraries
+import argparse
 import os
 import re
-import argparse
 
 
 # modify filename
-def rename_convension(filename):
+def rename_convension(filename, replace, replacement):
 
-    # capitalize only first letter
+    # replace name
     modified = filename
-    intermediar = list(modified)
-    intermediar[0] = intermediar[0].upper()
-    modified = "".join(intermediar)
+    modified = modified.replace(replace, replacement)
 
     # remove paranthese and text between them
     modified = re.sub(r'\([^)]*\)', '', modified)
@@ -27,6 +25,8 @@ def rename_convension(filename):
 # traverse files and print them
 def traverse(
         folder_path,
+        replace,
+        replacement,
         recursive=False,
         exceptions=tuple(),
         only=None,
@@ -58,19 +58,19 @@ def traverse(
                                 flag = True
                                 break
                     if not flag:
-                        print("[+] Renaming file '" + name + "'(from subpath '" + path + "') as '" + rename_convension(name) + "'")
+                        print("[+] Renaming file '" + name + "'(from subpath '" + path + "') as '" + rename_convension(name,replace,replacement) + "'")
                         file_path = os.path.join(path, name)
-                        new_name = os.path.join(path, rename_convension(name))
+                        new_name = os.path.join(path, rename_convension(name,replace,replacement))
                         os.rename(file_path, new_name)
 
             # folders
             if (not only) or (only == "folders"):
                 for name in subdirs:
-                        print("[+] Renaming folder '" + name + "'(from subpath '" + path + "') as '" + rename_convension(name) + "'")
+                        print("[+] Renaming folder '" + name + "'(from subpath '" + path + "') as '" + rename_convension(name,replace,replacement) + "'")
                         file_path = os.path.join(path, name)
-                        new_name = os.path.join(path, rename_convension(name))
+                        new_name = os.path.join(path, rename_convension(name,replace,replacement))
                         os.rename(file_path, new_name)
-                        name = rename_convension(name)
+                        name = rename_convension(name,replace,replacement)
 
 # main
 if __name__ == "__main__":
@@ -78,11 +78,15 @@ if __name__ == "__main__":
     # set arguments
     parser = argparse.ArgumentParser(
         description='Bulk rename files and folder from PATH',
-        epilog='Example of usage: ./bulk.py ./website -r -e html css js -o files',
+        epilog='Example of usage: ./bulk.py ./website  oldname newname -r -e html css js -o files',
         add_help=True,
     )
     parser.add_argument('path', action="store", nargs="*", default=True,
                         help='*REQUIRED* path to a directory with files')
+    parser.add_argument('replace', action="store", nargs="*", default=True,
+                        help='*REQUIRED* name to replace')
+    parser.add_argument('replacement', action="store", nargs="*", default=True,
+                        help='*REQUIRED* replaced name')
     parser.add_argument('-e', '--exceptions', action="store", nargs="*", default=False,
                         help='Except from renaming given extensions')
     parser.add_argument('-r', '--recursive', action="store_true", default=False,
@@ -93,7 +97,7 @@ if __name__ == "__main__":
 
     if hasattr(args, 'path') and isinstance(args.path, list):
         script_name = __file__.lstrip('.').lstrip('/')
-        traverse(args.path[0], args.recursive, args.exceptions, args.only,
+        traverse(args.path[0], args.path[1], args.path[2], args.recursive, args.exceptions, args.only,
                  script_name=script_name)
     else:
         parser.print_help()
